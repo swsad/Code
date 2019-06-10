4922a9692304423ba678365fa69c789c8ea9e387
fantasticgold <fantasticgold@foxmail.com>
2019-06-10 20:07:58 +0800

    [QA.wxml] [class: id style: height=100rpx] -> [class: weui style: height=100rpx]

--- miniprogram/pages/yaoxh6/QA/QA.js
+++ miniprogram/pages/yaoxh6/QA/QA.js
@@ -10,7 +10,8 @@ Page({
     answerIcon: "../../../images/price.png",
     questionIcon: "../../../images/price.png",
     supportIcon: "../../../images/support1.png",
-    question:''
+    question: "",
+    description: ""
   },
 
   /**
@@ -70,43 +71,26 @@ Page({
   },
 
   complete:function(){
+    console.log("complete")
     console.log(this.data.question);
+    console.log(this.data.description)
     if(this.data.question == ''){
       wx.showToast({
         title: '输入不能为空',
         icon:'none'
       })
     }
-    // wx.cloud.callFunction({
-    //   name: 'release_questionnaire',
-    //   data: {
-    //     name: '',
-    //     time: util.questionnaireDate(new Date()),
-    //     category: 'c2',
-    //     reward: 20,
-    //     position: '',
-    //     total_amount: 100,
-    //     content: JSON.stringify(this.data.questionnaireArray),
-    //     description: this.data.descriptionContent
-    //   },
-    //   success: res => {
-    //     wx.showToast({
-    //       title: '调用成功',
-    //     })
-    //   },
-    //   fail: err => {
-    //     wx.showToast({
-    //       icon: 'none',
-    //       title: '调用失败',
-    //     })
-    //     console.error('[云函数] [releaseQuestionnaire] 调用失败：', err)
-    //   }
-    // })
   },
 
-  bindblur:function(input){
+  bindblurQuestion: function(input) {
     this.setData({
       question: input.detail.value
     });
+  },
+
+  bindblurDescription: function(input) {
+    this.setData({
+      description: input.detail.value
+    })
   }
 })
